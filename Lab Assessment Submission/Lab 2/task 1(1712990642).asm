1c8d3b8151ed2afdbffef55416643fb81eda4f29
Faria Islam Hridy <44902188+FariaIslamHridy@users.noreply.github.com>
2020-11-18 09:21:06 +0600

    Task 1 done

---
+++ Lab Assessment Submission/Lab 2/task 1(1712990642).asm
@@ -0,0 +1,20 @@
+org 100h      
+            
+K1 EQU 2    ;Allocate first constant value
+K2 EQU 4    ;Allocate second constant value
+K3 EQU 6    ;Allocate third constant value
+
+
+MOV AX, K1  ;Allocate first value    
+MOV BX, AX  ;Add and store sum in BX register
+                                              
+                                       
+MOV AX, k2  ;Allocate second value    
+ADD BX, AX  ;Add and store sum in BX register
+
+
+MOV AX, k3  ;Allocate third value    
+ADD BX, AX  ;Add and store sum in BX register
+
+
+ret
